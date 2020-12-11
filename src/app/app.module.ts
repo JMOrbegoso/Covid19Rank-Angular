3ffb373054ae6365b3e06c4cd45e169924e0658d
JMOrbegoso <dev@jmorbegoso.com>
2020-12-11 07:42:35 -0500

    Created footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountryDetailComponent } from './country-detail/country-detail.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CountryDetailComponent
+    CountryDetailComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
