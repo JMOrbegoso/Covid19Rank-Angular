96afa27a3323112347efaa11b187bd3b60f1a23f
JMOrbegoso <dev@jmorbegoso.com>
2020-12-11 08:48:27 -0500

    Updated selector property of country-detail component

--- src/app/country-detail/country-detail.component.ts
+++ src/app/country-detail/country-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-country-detail',
+  selector: 'country-detail',
   templateUrl: './country-detail.component.html',
   styleUrls: ['./country-detail.component.css']
 })
