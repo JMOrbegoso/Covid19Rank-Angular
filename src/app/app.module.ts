31c1f98ca4a2b0424757b5f537c9913a8a1e71e3
JMOrbegoso <dev@jmorbegoso.com>
2020-12-11 07:32:49 -0500

    Created country-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountryDetailComponent } from './country-detail/country-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountryDetailComponent
   ],
   imports: [
     BrowserModule,
