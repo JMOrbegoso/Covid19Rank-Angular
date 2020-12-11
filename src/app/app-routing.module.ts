880fa53f550dd70980a8b9ca495ea52d804218f9
JMOrbegoso <dev@jmorbegoso.com>
2020-12-11 16:14:28 -0500

    Updated app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CountryDetailComponent } from './country-detail/country-detail.component';
+import { RankComponent } from './rank/rank.component';
+
+const routes: Routes = [
+  { path: '', component: RankComponent, pathMatch:'full', },
+  { path: 'country/:countryName', component: CountryDetailComponent, },
+  { path: '**', redirectTo:'', pathMatch:'full', }, //<-- It can be a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
+
 export class AppRoutingModule { }
