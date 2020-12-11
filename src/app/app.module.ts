8852d4091f2ce21d7ed16990a17236a858d71401
JMOrbegoso <dev@jmorbegoso.com>
2020-12-11 08:09:38 -0500

    Removed footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountryDetailComponent } from './country-detail/country-detail.component';
-import { FooterComponent } from './footer/footer.component';
 import { NavHeaderComponent } from './nav-header/nav-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CountryDetailComponent,
-    FooterComponent,
     NavHeaderComponent
   ],
   imports: [
