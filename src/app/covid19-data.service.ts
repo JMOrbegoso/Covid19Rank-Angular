468fb777308bae6e9fa680ca5c4698f57069b9b7
JMOrbegoso <dev@jmorbegoso.com>
2020-12-11 21:07:30 -0500

    Updated covid19-data.service.ts

--- src/app/covid19-data.service.ts
+++ src/app/covid19-data.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Observable } from 'rxjs';
+
+import { Country, CountryHistorical } from './covid19-data.types';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,5 +12,17 @@ export class Covid19DataService {
 
   constructor(private httpClient: HttpClient) {
     
+   }
+
+   getCountries(): Observable<Country[]>{
+    return this.httpClient.get<Country[]>("https://disease.sh/v2/countries?yesterday=false");
+   }
+
+   getCountry(countryName: string): Observable<Country[]>{
+    return this.httpClient.get<Country[]>("https://disease.sh/v2/countries/" + countryName + "?yesterday=true");
+   }
+
+   getCountryHistorical(countryName: string): Observable<CountryHistorical>{
+    return this.httpClient.get<CountryHistorical>("https://disease.sh/v2/historical/" + countryName + "?lastdays=all");
    }
 }
