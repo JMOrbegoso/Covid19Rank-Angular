196f7645533142ca35b27fe3e00861fd26dc90fe
JMOrbegoso <dev@jmorbegoso.com>
2020-12-13 16:52:36 -0500

    Updated historical-line-chart

--- src/app/historical-line-chart/historical-line-chart.component.ts
+++ src/app/historical-line-chart/historical-line-chart.component.ts
@@ -1,19 +1,65 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
-import { CountryHistorical } from '../covid19-data.types';
+import { Chart, ChartOptions, } from 'chart.js';
+
+import { HistoricalValue } from '../covid19-data.types';
 
 @Component({
   selector: 'historical-line-chart',
   templateUrl: './historical-line-chart.component.html',
   styleUrls: ['./historical-line-chart.component.css']
 })
-export class HistoricalLineChartComponent implements OnInit {
+export class HistoricalLineChartComponent implements OnInit, OnChanges {
 
-  @Input() countryHistorical: CountryHistorical | any;
+	@Input() historicalData : Map<string, HistoricalValue> | undefined;
+ 
+	chart: Chart | undefined = undefined;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	};
 
+	ngOnChanges(changes: SimpleChanges): void
+	{
+		if(this.chart !== undefined){
+			this.chart.destroy();
+		}		
+
+		const context: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('canvas');
+
+		const historicalData : Map<string, HistoricalValue> = (<Map<string, HistoricalValue>>this.historicalData);
+
+		const options : ChartOptions = {
+			tooltips: {	enabled: true,	},
+			legend: {
+				display: true,
+				position: 'bottom',
+				labels: { fontColor: 'black' },
+			},
+			scales: {
+				yAxes: [
+					{ ticks: { fontColor: "black", display: true, beginAtZero: true, }, },
+				],
+				xAxes: [
+					{ ticks: { fontColor: "black", display: true, }, },
+				],
+			},
+		};
+		
+		const configuration : Chart.ChartConfiguration = {
+			type: 'line',
+			data: {
+				labels: [...historicalData.keys()],
+				datasets: [
+					{ label: "infected", fill: false, backgroundColor: '#FF0000', borderColor: '#FF0000', data: [...historicalData.values()].map(item => item.infected), },
+					{ label: "deaths", fill: false, backgroundColor: '#000000', borderColor: '#000000', data: [...historicalData.values()].map(item => item.deceased ), },
+					{ label: "recovered", fill: false, backgroundColor: '#102EF3D9', borderColor: '#102EF3D9', data: [...historicalData.values()].map(item => item.recovered ), },
+				],
+			},
+			options: options,
+		};
+
+		this.chart = new Chart(context, configuration);
+	}
 }
