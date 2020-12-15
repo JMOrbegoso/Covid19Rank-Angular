0c9e71c639a09a5602c35f9252e7184f12ace4e2
JMOrbegoso <dev@jmorbegoso.com>
2020-12-15 18:59:34 -0500

    Updated app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CountryDetailComponent } from './country-detail/country-detail.component';
-import { RankComponent } from './rank/rank.component';
+import { LatinAmericanRankingComponent } from './latin-american-ranking/latin-american-ranking.component';
 
 const routes: Routes = [
-  { path: '', component: RankComponent, pathMatch:'full', },
+  { path: '', component: LatinAmericanRankingComponent, pathMatch:'full', },
   { path: 'country/:countryName', component: CountryDetailComponent, },
   { path: '**', redirectTo:'', pathMatch:'full', }, //<-- It can be a 404 page
 ];
