cea1897092c42811c24db50fda47943fab3b5fc0
JMOrbegoso <dev@jmorbegoso.com>
2020-12-15 11:06:06 -0500

    Updated historical-lethality-line-chart.component.ts

--- src/app/historical-lethality-line-chart/historical-lethality-line-chart.component.ts
+++ src/app/historical-lethality-line-chart/historical-lethality-line-chart.component.ts
@@ -22,10 +22,40 @@ export class HistoricalLethalityLineChartComponent implements OnInit, OnChanges 
   {
     if(this.chart !== undefined){
 			this.chart.destroy();
-		}		
+		}
 
 		const context: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('canvas_historical_lethality_line_chart');
 
-		const historicalData : Map<string, number> = (<Map<string, number>>this.lethalityHistoricalData);
-	}
+    const historicalData : Map<string, number> = (<Map<string, number>>this.lethalityHistoricalData);
+    
+    const options : ChartOptions = {
+			tooltips: {	enabled: true,	},
+			legend: {
+				display: true,
+				position: 'bottom',
+				labels: { fontColor: 'black' },
+			},
+			scales: {
+				yAxes: [
+					{ ticks: { fontColor: "black", display: true, beginAtZero: true, }, },
+				],
+				xAxes: [
+					{ ticks: { fontColor: "black", display: true, }, },
+				],
+			},
+		};
+
+		const configuration : Chart.ChartConfiguration = {
+			type: 'line',
+			data: {
+				labels: [...historicalData.keys()],
+				datasets: [
+					{ label: "Porcentaje de letalidad", fill: false, backgroundColor: '#FFFFFF', borderColor: '#FF0000', data: [...historicalData.values()].map(item => item), },
+				],
+			},
+      options: options,
+		};
+
+		this.chart = new Chart(context, configuration);
+	};
 }
